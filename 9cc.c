fe2dbfc40cf98990b75e8f8cd064dac2fe9a02d6
Sanshiro Matsumoto <samatsum@c6r4s6.42tokyo.jp>
2025-01-23 11:24:44 +0900

    足し算と引き算を追加

--- 9cc.c
+++ 9cc.c
@@ -1,5 +1,3 @@
-// .intel_syntax noprefix
-
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -9,10 +7,46 @@ int main(int argc, char **argv) {
     return 1;
   }
 
+  char *p = argv[1];
+
   printf(".intel_syntax noprefix\n");
   printf(".globl main\n");
   printf("main:\n");
-  printf("  mov rax, %d\n", atoi(argv[1]));
+  printf("  mov rax, %ld\n", strtol(p, &p, 10));
+
+  while (*p) {
+    if (*p == '+') {
+      p++;
+      printf("  add rax, %ld\n", strtol(p, &p, 10));
+      continue;
+    }
+
+    if (*p == '-') {
+      p++;
+      printf("  sub rax, %ld\n", strtol(p, &p, 10));
+      continue;
+    }
+
+    fprintf(stderr, "予期しない文字です: '%c'\n", *p);
+    return 1;
+  }
+
   printf("  ret\n");
   return 0;
 }
+// #include <stdio.h>
+// #include <stdlib.h>
+
+// int main(int argc, char **argv) {
+//   if (argc != 2) {
+//     fprintf(stderr, "引数の個数が正しくありません\n");
+//     return 1;
+//   }
+
+//   printf(".intel_syntax noprefix\n");
+//   printf(".globl main\n");
+//   printf("main:\n");
+//   printf("  mov rax, %d\n", atoi(argv[1]));
+//   printf("  ret\n");
+//   return 0;
+// }
